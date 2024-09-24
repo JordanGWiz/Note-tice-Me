16ba2aa6d1c53926d087069e4837d8d6a40b37fc
Jordan <jordan.gonzales17@hotmail.com>
2024-09-24 04:26:17 -0700

    adding the server.js file

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
+const apiRoutes = require("./routes/notes");
+const htmlRoutes = require("./routes/html");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.use("/api/notes", apiRoutes);
+app.use("/", htmlRoutes);
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}`);
+});
